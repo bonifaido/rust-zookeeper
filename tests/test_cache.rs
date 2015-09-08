4624823b7cf8fb62f9b427d250061d158291128e
Nándor István Krácser <bonifaido@gmail.com>
2015-09-08 10:55:32 +0200

    Update test_cache.rs

--- tests/test_cache.rs
+++ tests/test_cache.rs
@@ -1,5 +1,5 @@
 use zookeeper::CreateMode::*;
-use zookeeper::{WatchedEvent, ZooKeeper};
+use zookeeper::{WatchedEvent, ZooKeeper, ZooKeeperExt};
 use zookeeper::acls::*;
 use zookeeper::recipes::cache::PathChildrenCache;
 
@@ -16,13 +16,14 @@ fn path_children_cache_test() {
     let _ = env_logger::init();
 
     // Create a test cluster
-    //let cluster = ZkCluster::start(3);
+    let cluster = ZkCluster::start(1);
 
     // Connect to the test cluster
-    let zk = Arc::new(ZooKeeper::connect("localhost:2181", //&cluster.connect_string,
+    let zk = Arc::new(ZooKeeper::connect(&cluster.connect_string,
                                          Duration::from_secs(30),
                                          |event: &WatchedEvent| info!("{:?}", event)).unwrap());
 
+    zk.ensure_path("/cache").unwrap();
     zk.create("/cache/a", vec![1,4], OPEN_ACL_UNSAFE.clone(), Ephemeral).unwrap();
     zk.create("/cache/b", vec![2,4], OPEN_ACL_UNSAFE.clone(), Ephemeral).unwrap();
     zk.create("/cache/c", vec![3,4], OPEN_ACL_UNSAFE.clone(), Ephemeral).unwrap();
