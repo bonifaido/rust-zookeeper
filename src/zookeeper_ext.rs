64aaab93ba609ced63a3b1b01d86b1ef67a20b89
Nándor István Krácser <bonifaido@gmail.com>
2015-09-08 10:37:32 +0200

    Fix ensure_path

--- src/zookeeper_ext.rs
+++ src/zookeeper_ext.rs
@@ -1,6 +1,7 @@
 use acls::*;
 use consts::{CreateMode, ZkError};
 use zookeeper::{ZkResult, ZooKeeper};
+use std::iter::once;
 
 pub trait ZooKeeperExt {
     fn ensure_path(&self, path: &str) -> ZkResult<()>;
@@ -8,7 +9,7 @@ pub trait ZooKeeperExt {
 
 impl ZooKeeperExt for ZooKeeper {
     fn ensure_path(&self, path: &str) -> ZkResult<()> {
-        for (i, _) in path.chars().enumerate().skip(1).filter(|c| c.1 == '/') {
+        for (i, _) in path.chars().chain(once('/')).enumerate().skip(1).filter(|c| c.1 == '/') {
             match self.create(&path[..i], vec![], OPEN_ACL_UNSAFE.clone(), CreateMode::Persistent) {
                 Ok(_) | Err(ZkError::NodeExists) => {},
                 Err(e) => return Err(e)
