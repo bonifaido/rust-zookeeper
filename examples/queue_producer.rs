3f7eb046c7974543c5cee6240b59b428ce95ee28
Colum <colum.mcgaley@fastmail.com>
2021-02-06 09:01:29 -0800

    First pass at Zookeeper Queue

---
+++ examples/queue_producer.rs
@@ -0,0 +1,38 @@
+extern crate env_logger;
+extern crate uuid;
+extern crate zookeeper;
+
+use std::{env, sync::Arc, thread, time::Duration};
+
+use zookeeper::{recipes::queue::ZkQueue, WatchedEvent, Watcher, ZooKeeper};
+
+struct NoopWatcher;
+
+impl Watcher for NoopWatcher {
+    fn handle(&self, _ev: WatchedEvent) {}
+}
+
+fn zk_server_urls() -> String {
+    let key = "ZOOKEEPER_SERVERS";
+    match env::var(key) {
+        Ok(val) => val,
+        Err(_) => "localhost:2181".to_string(),
+    }
+}
+
+fn main() {
+    env_logger::init();
+
+    let zk_urls = zk_server_urls();
+    log::info!("connecting to {}", zk_urls);
+
+    let zk = ZooKeeper::connect(&*zk_urls, Duration::from_millis(2500), NoopWatcher).unwrap();
+
+    let queue = ZkQueue::new(Arc::new(zk), "/testing2".to_string());
+
+
+    let message = "Hello World";
+    let op = queue.offer(Vec::from(message.as_bytes()));
+    println!("{:?}", op);
+
+}
---
+++ src/recipes/queue.rs
@@ -0,0 +1,106 @@
+/// https://github.com/apache/zookeeper/blob/master/zookeeper-recipes/zookeeper-recipes-queue/src/main/java/org/apache/zookeeper/recipes/queue/DistributedQueue.java
+///
+use crate::{
+    paths, Acl, CreateMode, Subscription, WatchedEvent, WatchedEventType, ZkError, ZkResult,
+    ZkState, ZooKeeper,
+    watch::Watcher
+};
+use std::sync::mpsc::{SyncSender, Receiver, sync_channel};
+use std::sync::Arc;
+
+/// The default prefix to use for all children in the znode
+/// should be the same as the example recipe: https://github.com/apache/zookeeper/blob/245ff759b0e9fe0a1815e03433306ac805bf5e95/zookeeper-recipes/zookeeper-recipes-queue/src/main/java/org/apache/zookeeper/recipes/queue/DistributedQueue.java#L48
+const ZK_DISTRIBUTEDQUEUE_PREFIX: &str = "qn-";
+
+pub struct ZkQueue {
+    dir: String,
+    zk: Arc<ZooKeeper>,
+}
+impl ZkQueue {
+    pub fn new(zk: Arc<ZooKeeper>, dir: String) -> Self {
+        if zk.exists(&dir, false).unwrap().is_none() {
+            println!("does mot exist");
+            zk.create(&dir, vec![0], Acl::open_unsafe().clone(), CreateMode::Container).unwrap();
+        }
+        Self {
+            zk,
+            dir
+        }
+    }
+
+    /// Inserts data into the queue
+    pub fn offer(&self, data: Vec<u8>) -> ZkResult<String> {
+        self.zk.create(
+            &*format!("{}/{}", self.dir, ZK_DISTRIBUTEDQUEUE_PREFIX),
+            data,
+            Acl::open_unsafe().clone(),
+            CreateMode::PersistentSequential)
+    }
+
+    fn claim(&self, key: String) -> ZkResult<Vec<u8>> {
+        debug!("claiming {}", &key);
+        let data = self.zk.get_data(&key, false)?;
+        self.zk.delete(&key, None)?;
+        Ok(data.0)
+    }
+
+    /// Returns a Vec of the children, in
+    fn ordered_children<W: Watcher + 'static>(&self, watcher: Option<W>) -> ZkResult<Vec<String>> {
+        let mut children: Vec<(u64, String)> = Vec::new();
+        match watcher {
+            Some(W) => self.zk.get_children_w(&self.dir, W),
+            None => self.zk.get_children(&self.dir, false) // false I think?
+        }?.iter().for_each(|child| {
+            if let Ok(index) = child.replace(ZK_DISTRIBUTEDQUEUE_PREFIX, "").parse::<u64>() {
+                children.push((index, child.clone()))
+            } else {
+                warn!("found child with improper name: {}", child);
+            }
+        });
+
+        children.sort_by(|a, b| a.0.cmp(&b.0));
+
+        Ok(children.iter().map(|i| i.1.clone()).collect())
+    }
+
+    /// Removes the head of the queue and returns it, blocking until it succeeds or throws an error
+    pub fn take(&self) -> ZkResult<Vec<u8>> {
+        // call self.ordered_children
+        let latch: (SyncSender<bool>, Receiver<bool>) = sync_channel(1);
+        loop {
+            let tx = latch.0.clone();
+            let op = self.ordered_children(Some(move |ev| {
+                handle_znode_change(&tx, ev)
+            }))?;
+
+            // if we get keys from self.ordered_children, return the top key after claiming it
+            if op.len() > 0 {
+                let claim = self.claim(format!("{}/{}", self.dir, op[0]));
+                if claim.is_err() {
+                    let error = claim.err().unwrap();
+                    // if there is no node, we want to try this whole process again
+                    // if there is an error, abort because something is broken
+                    if ZkError::NoNode != error { return Err(error); }
+                    continue;
+                }
+                return Ok(claim.unwrap());
+            }
+
+            // otherwise, wait until we get something back from the Reciver
+            let _ = latch.1.recv().unwrap();
+        }
+        // infinite recursion for the win
+    }
+    //
+    // pub fn remove(&self) -> ZkResult<Option<Vec<u8>>> {
+    //
+    // }
+    //
+
+}
+
+fn handle_znode_change(chan: &SyncSender<bool>, ev: WatchedEvent) {
+    if let WatchedEventType::NodeChildrenChanged = ev.event_type {
+        let _ = chan.send(true);
+    }
+}
